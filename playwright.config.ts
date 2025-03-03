b3e466d5326040e06a4292bcd13ba21f5ca5e6d2
yashwanth-cyberdev <yashgowda195@gmail.com>
2025-03-03 21:15:50 +0530

    test: Added basic login test and header assertion

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices, PlaywrightTestConfig } from "@playwright/test";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-const config = {
+const config: PlaywrightTestConfig = {
   testDir: "./tests",
+  expect: {
+    timeout: 6000,
+  },
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- tests/basic-ui-test.spec.ts
+++ tests/basic-ui-test.spec.ts
@@ -12,3 +12,14 @@ test("Page playwright test", async ({ page }) => {
   await page.goto("https://google.com");
   await expect(page).toHaveTitle("Google");
 });
+
+test("Playwright login test", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill("testuserengineer@example.com");
+  await page.locator("#userPassword").fill("Password1@");
+  await page.locator("#login").click();
+  await page.waitForLoadState("networkidle");
+  await expect(page.locator(".logo-holder p")).toContainText(
+    "Automation Practice"
+  );
+});
